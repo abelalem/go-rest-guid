dca9583649bb0445c8e2761bec9c1609e2d130a7
Alemayehu Ewunetu <alemayehu.ewunetu@eaglelionsystems.com>
2025-05-13 13:46:48 +0300

    Implement the Recipes REST handlers
    * Defined the data model
    * Defined a storage interface
    * Implemented generic error handlers

--- cmd/standardlib/main.go
+++ cmd/standardlib/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 	"regexp"
+
+	"github.com/abelalem/go-rest-guid/pkg/recipes"
 )
 
 type homeHandler struct{}
@@ -16,7 +18,33 @@ var (
 	RecipeRequestWithID = regexp.MustCompile("^/recipes/([a-z0-9]+(?:-[a-z0-9]+)+)$")
 )
 
-type RecipesHandler struct{}
+func InternalServerErrorHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("500 Internal Server Error"))
+}
+
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("404 Not Found"))
+}
+
+type recipeStore interface {
+	Add(name string, recipe recipes.Recipe) error
+	Get(name string) (recipes.Recipe, error)
+	Update(name string, recipe recipes.Recipe) error
+	List() (map[string]recipes.Recipe, error)
+	Remove(name string) error
+}
+
+type RecipesHandler struct {
+	store recipeStore
+}
+
+func NewRecipesHandler(s recipeStore) *RecipesHandler {
+	return &RecipesHandler{
+		store: s,
+	}
+}
 
 func (h *RecipesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch {
@@ -48,14 +76,18 @@ func (h *RecipesHandler) DeleteRecipe(w http.ResponseWriter, r *http.Request) {}
 
 func main() {
 
+	// Create the store and Recipe Handler
+	store := recipes.NewMemStore()
+	recipesHandler := NewRecipesHandler(store)
+
 	// Create a new request multiplexer
 	// Take incoming requests and dispatch them to the matching handlers
 	mux := http.NewServeMux()
 
 	// Register the routers and handlers
 	mux.Handle("/", &homeHandler{})
-	mux.Handle("/recipes", &RecipesHandler{})
-	mux.Handle("/recipes/", &RecipesHandler{})
+	mux.Handle("/recipes", recipesHandler)
+	mux.Handle("/recipes/", recipesHandler)
 
 	// Run the server
 	http.ListenAndServe(":8080", mux)
---
+++ pkg/recipes/models.go
@@ -0,0 +1,12 @@
+package recipes
+
+// Represents individual ingredients
+type Ingredient struct {
+	Name string `json:"name"`
+}
+
+// Represents a recipe
+type Recipe struct {
+	Name string `json:"name"`
+	Ingredients []Ingredient `json:"name"`
+}
