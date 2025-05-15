46467684e3cc3152aac178f8fd2c4272e9682eb8
Alemayehu Ewunetu <alemayehu.ewunetu@eaglelionsystems.com>
2025-05-15 12:45:57 +0300

    Build a REST API with gorilla/mux
    * Implemented handlers

--- cmd/gorilla/main.go
+++ cmd/gorilla/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/abelalem/go-rest-guid/pkg/recipes"
 	"github.com/gorilla/mux"
+	"github.com/gosimple/slug"
 )
 
 type homeHandler struct{}
@@ -21,6 +23,16 @@ type recipeStore interface {
 	Remove(name string) error
 }
 
+func InternalServerErrorHandler(rw http.ResponseWriter, r *http.Request) {
+	rw.WriteHeader(http.StatusInternalServerError)
+	rw.Write([]byte("500 Internal Server Error"))
+}
+
+func NotFoundHandler(rw http.ResponseWriter, r *http.Request) {
+	rw.WriteHeader(http.StatusNotFound)
+	rw.Write([]byte("404 Not Found"))
+}
+
 type RecipesHandler struct {
 	store recipeStore
 }
@@ -31,11 +43,97 @@ func NewRecipesHandler(s recipeStore) *RecipesHandler {
 	}
 }
 
-func (h RecipesHandler) CreateRecipe(rw http.ResponseWriter, r *http.Request) {}
-func (h RecipesHandler) ListRecipes(rw http.ResponseWriter, r *http.Request)  {}
-func (h RecipesHandler) GetRecipe(rw http.ResponseWriter, r *http.Request)    {}
-func (h RecipesHandler) UpdateRecipe(rw http.ResponseWriter, r *http.Request) {}
-func (h RecipesHandler) DeleteRecipe(rw http.ResponseWriter, r *http.Request) {}
+func (h RecipesHandler) CreateRecipe(rw http.ResponseWriter, r *http.Request) {
+	// Recipe object that will be populated from json payload
+	var recipe recipes.Recipe
+
+	if err := json.NewDecoder(r.Body).Decode(&recipe); err != nil {
+		InternalServerErrorHandler(rw, r)
+		return
+	}
+
+	resourceID := slug.Make(recipe.Name)
+
+	if err := h.store.Add(resourceID, recipe); err != nil {
+		InternalServerErrorHandler(rw, r)
+		return
+	}
+
+	rw.WriteHeader(http.StatusOK)
+}
+func (h RecipesHandler) ListRecipes(rw http.ResponseWriter, r *http.Request) {
+	recipesList, err := h.store.List()
+	if err != nil {
+		InternalServerErrorHandler(rw, r)
+		return
+	}
+
+	jsonBytes, err := json.Marshal(recipesList)
+	if err != nil {
+		InternalServerErrorHandler(rw, r)
+		return
+	}
+
+	rw.WriteHeader(http.StatusOK)
+	rw.Write(jsonBytes)
+}
+func (h RecipesHandler) GetRecipe(rw http.ResponseWriter, r *http.Request) {
+	// Get recipeId from the request
+	recipeId := mux.Vars(r)["id"]
+
+	recipe, err := h.store.Get(recipeId)
+	if err != nil {
+		if err == recipes.NotFoundErr {
+			NotFoundHandler(rw, r)
+			return
+		}
+
+		InternalServerErrorHandler(rw, r)
+		return
+	}
+
+	jsonBytes, err := json.Marshal(recipe)
+	if err != nil {
+		InternalServerErrorHandler(rw, r)
+		return
+	}
+
+	rw.WriteHeader(http.StatusOK)
+	rw.Write(jsonBytes)
+}
+func (h RecipesHandler) UpdateRecipe(rw http.ResponseWriter, r *http.Request) {
+	// Get recipeId from the request
+	recipeId := mux.Vars(r)["id"]
+
+	// Recipe object that will be populated from JSON payload
+	var recipe recipes.Recipe
+	if err := json.NewDecoder(r.Body).Decode(&recipe); err != nil {
+		InternalServerErrorHandler(rw, r)
+		return
+	}
+
+	if err := h.store.Update(recipeId, recipe); err != nil {
+		if err == recipes.NotFoundErr {
+			NotFoundHandler(rw, r)
+			return
+		}
+		InternalServerErrorHandler(rw, r)
+		return
+	}
+
+	rw.WriteHeader(http.StatusOK)
+}
+func (h RecipesHandler) DeleteRecipe(rw http.ResponseWriter, r *http.Request) {
+	// Get recipeId from the request
+	recipeId := mux.Vars(r)["id"]
+
+	if err := h.store.Remove(recipeId); err != nil {
+		InternalServerErrorHandler(rw, r)
+		return
+	}
+
+	rw.WriteHeader(http.StatusOK)
+}
 
 func main() {
 	// Create the store and Recipe Handler
